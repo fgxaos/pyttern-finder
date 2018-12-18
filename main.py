203fa8181935b393e935c6fd2eb1e23f3224e8b4
xaos <francois.garderes@student.ecp.fr>
2018-12-18 20:02:20 +0100

    Adapted to the new workflow

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ params = {
 pylab.rcParams.update(params)
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
-
+"""
 model_methods = [
     ['googlenet', 'vanilla_grad', 'imshow'], 
     ['googlenet', 'grad_x_input', 'imshow'], 
@@ -32,18 +32,32 @@ model_methods = [
     ['vgg16', 'pattern_lrp', 'imshow'],
     ['resnet50', 'real_time_saliency', 'camshow']
 ]
+"""
+## Add more model_methods if it works
+model_methods = [
+    ['squeezenet_8_12', 'vanilla_grad', 'imshow'],
+]
+
+# Change 'displayed_class' to "dog" if you want to display for a dog
+displayed_class = "cat"
 
 # Take the sample image, and display it (original form)
-image_path = 'images/elephant.png'
-image_class = 101   # tusker
+if displayed_class == "cat":
+    image_path= 'models/cat_eating_fancy_ice_cream.jpg.838x0_q80.jpg'
+elif displayed_class == "dog":
+    image_path_dog = 'models/Natural-Dog-Law-2-To-dogs,-energy-is-everything.jpg'
+else:
+    print("Bad value for 'displayed_class', please check it")
+
 raw_img = viz.pil_loader(image_path)
 plt.figure(figsize=(5,5))
 plt.imshow(raw_img)
 plt.axis('off')
-plt.title('class_id=101 (Tusker)')
+plt.title("Displayed class: ", displayed_class)
 
 # Now, we want to display the saliency maps of this image, for every model_method element
 all_saliency_maps = []
+
 for model_name, method_name, _ in model_methods:
     # Get a specific picture transformation (see torchvision.transforms documentation) 
     transf = get_preprocess(model_name, method_name)
@@ -66,12 +80,13 @@ for model_name, method_name, _ in model_methods:
     saliency = utils.upsample(saliency, (raw_img.height, raw_img.width))
     all_saliency_maps.append(saliency.cpy().numpy())
 
+
 # Display all the results
 plt.figure(figsize=(25, 15))
 plt.subplot(3, 5, 1)
 plt.imshow(raw_img)
 plt.axis('off')
-plt.title('class_id=101 (Tusker)')
+plt.title('Displayed class: ', displayed_class)
 for i, (saliency, (model_name, method_name, show_style)) in enumerate(zip(all_saliency_maps, model_methods)):
     plt.subplot(3, 5, i + 2 + i // 4)
     if show_style == 'camshow':
@@ -94,20 +109,5 @@ for i, (saliency, (model_name, method_name, show_style)) in enumerate(zip(all_sa
         plt.title('%s' % (method_name))
 
 plt.tight_layout()
-plt.savefig('images/tusker_saliency.png')
-
-
-
-
-"""
-Currently, have a problem with loading the GoogleNet
-    => because of a change of version, '.' are now forbidden in the names of the 'convX()' (etc.)
-    => but if I change the '.', error in loading state_dict for GoogleNet (missing keys + unexpected keys in state_dict)
-So, what should I do?
-
-If there is no solution, choose another model
-
-There might be a solution in the "Model transfer" tutorial: perhaps it has changed over the versions, check how to do it
-    => study the structure of the GoogleNet
-    => deduce a new model transfer algorithm
-"""
+save_destination = 'images/' + displayed_class + '_saliency.png'
+plt.savefig(save_destination)
