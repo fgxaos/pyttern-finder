bcb91200b8c03f9ca201d667be6bd1661030ecd6
xaos <francois.garderes@student.ecp.fr>
2019-01-12 01:14:23 +0100

    Correction for Squeezenet

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 from torchvision import models
 from torch.autograd import Variable
 from torch._thnn import type2backend
+import torch
+
+# Model classes
+from models.model_classes import SqueezeNet
 
         
 def load_model(arch):
@@ -18,18 +22,19 @@ def load_model(arch):
         from googlenet import get_googlenet
         model = get_googlenet(pretrain=True)
     elif model_name == 'alexnet':
-        
+        print("Nothing happened")
     elif model_name == 'vgg':
-    
+        print("Nothing happened")
     elif model_name == 'resnet':
-    
+        print("Nothing happened")
     elif model_name == 'squeezenet':
-        model_path = "./models/model_saves/squeezenet_" + str(batch_size) + "_" + str(number_epochs)
-        model = model.import(model_path)
+        model_path = "./models/model_saves/squeezenet_" + str(batch_size) + "_" + str(number_epochs) + ".pth"
+        model = SqueezeNet(version=1.0)
+        model.load_state_dict(torch.load(model_path))
     elif model_name == 'densenet':
-
+        print("Nothing happened")
     elif model_name == 'inception':
-
+        print("Nothing happened")
     else:
         model = models.__dict__[arch](pretrained=True)
     model.eval()
@@ -49,7 +54,10 @@ def upsample(inp, size):
     backend = type2backend[inp.type()]
     f = getattr(backend, 'SpatialUpSamplingBilinear_updateOutput')
     upsample_inp = inp.new()
-    f(backend.library_state, inp, upsample_inp, size[0], size[1])
+    # The last argument stands for "align_corners" (if True, the corner pixels of the input 
+    # and output tensors are aligned, and thus preserving the values at those pixels). This 
+    # only has effect when :attr:`mode` is `linear`, `bilinear` or `trilinear` (default: False)
+    f(backend.library_state, inp, upsample_inp, size[0], size[1], False)
     return upsample_inp
 
 
@@ -57,10 +65,10 @@ def extract_info_from_name(arch_name):
     # Architecture is named using the following code: 
     # saving_model_name = model_name + "_" + str(batch_size) + "_" + str(num_epochs) + ".pth"
     list_names = arch_name.split("_")
-    if(len(list_names < 3)):
+    if(len(list_names) < 3):
         return arch_name
     else:
-        model_name = list_name[0]
-        batch_size = list_name[1]
-        number_epochs = list_name[2][:-4]
+        model_name = list_names[0]
+        batch_size = list_names[1]
+        number_epochs = list_names[2]
         return model_name, batch_size, number_epochs
